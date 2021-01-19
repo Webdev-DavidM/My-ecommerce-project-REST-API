1080541d43aae71b6acd6dca6d6dcbb03d8d2021
David Mulholland <davidmulholland@Davids-iMac.local>
2021-01-19 11:51:50 +0000

    product item buttons added to the prduct page

--- front-end/src/Pages/Product.js
+++ front-end/src/Pages/Product.js
@@ -6,8 +6,8 @@ import ProductImageCarousel from '../Components/UIelements/ProductImageCarousel'
 export default class Product extends Component {
   state = {
     size: '',
+    dropdownSelected: false,
     quantity: 0,
-    AddToBasket: false,
   };
 
   handleChange(event) {
@@ -20,6 +20,13 @@ export default class Product extends Component {
   }
 
   render() {
+    let dropdown = (
+      <>
+        <option>small</option>
+        <option>medium</option>
+        <option>large</option>
+      </>
+    );
     return (
       <div className={styles.product}>
         <p className={styles.title}>Vitus road bike</p>
@@ -28,21 +35,58 @@ export default class Product extends Component {
         <ProductImageCarousel />
         <p className={styles.price}>£999.99</p>
         <p>Colour: Black Quartz</p>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <label>
-              Select a size
-              <select>
-                <option value='small'>small</option>
-                <option value='medium'>medium</option>
-                <option selected value='large'>
-                  large
-                </option>
-              </select>
-            </label>
-            <input type='submit' value='submit' />
+        <div></div>
+
+        <div className={styles.sizecontainer}>
+          <form
+            onClick={() =>
+              this.setState((prevState) => ({
+                dropdownSelected: !prevState.dropdownSelected,
+              }))
+            }>
+            Please select
+            {this.state.dropdownSelected && dropdown}
           </form>
         </div>
+        <br></br>
+        <div className={styles.quantity}>
+          <form>Quantity:</form>
+          <button
+            className={styles.qtybtn}
+            onClick={() =>
+              this.setState((prevState) => ({
+                quantity: prevState.quantity - 1,
+              }))
+            }>
+            -
+          </button>
+          <input type='input' value={this.state.quantity}></input>
+          <button
+            className={styles.qtybtn}
+            onClick={() =>
+              this.setState((prevState) => ({
+                quantity: prevState.quantity + 1,
+              }))
+            }>
+            +
+          </button>
+          <br></br>
+        </div>
+        <button className={styles.addtocartbtn}>
+          {' '}
+          <i
+            className='fas fa-shopping-cart'
+            style={{ width: '1rem', height: '1rem' }}>
+            {' '}
+          </i>
+          &nbsp; &nbsp;Add to Basket
+        </button>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad pariatur
+          nemo saepe, distinctio, maiores sapiente ea consequuntur odio sit,
+          voluptatibus iste aspernatur quis nisi! Expedita, unde! Minus deleniti
+          culpa odit. Praesentium eligendi illo doloremque nisi perferendis
+        </p>
       </div>
     );
   }
