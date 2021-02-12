fa466e9005e998e7d49df7d20c69fd36ae7903bf
David Mulholland <davidmulholland@Davids-iMac.local>
2021-02-12 16:55:45 +0000

    Revert "various UI improvements and fixes"
    This reverts commit eb2dfa6caa8349fbbf92656d7852eac095971cb5.

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -26,7 +26,6 @@ import jwtVerify from '../middleware/jwtVerify.js';
 app.post('/register', async (req, res) => {
   // Firstly I will make sure the user doesnt exist on the database already
   let existingUser = await User.find({ email: req.body.email });
-  console.log(existingUser);
   if (existingUser.length !== 0) {
     return res
       .status(401)
